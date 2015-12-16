8b17f23d5af4483351eb3be8d96da81b292798ef
Maxwell Friederichs <orngcrsh2010@gmail.com>
2015-12-16 10:38:22 -0600

    Working dockerfile

--- config/local.py
+++ config/local.py
@@ -1,11 +1,14 @@
 from .settings import *
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
-    }
-}
-
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'pokeapi',
+            'USER': 'ash',
+            'PASSWORD': 'pokemon',
+            'HOST': 'localhost',
+            'PORT': '',
+            }
+        }
 DEBUG = True
 TASTYPIE_FULL_DEBUG = True
